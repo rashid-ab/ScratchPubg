dba97395b8c75483876c332f5ec20959e6df985b
unknown <Rashidbuttuog@gmail.com>
2021-08-14 08:41:41 -0700

    Id

--- app/User.php
+++ app/User.php
@@ -15,7 +15,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password','uc','coins','redeem_uc','status'
+        'name', 'email', 'password','uc','coins','redeem_uc','pubg_id','status'
     ];
 
     /**
